293bd6a72a6768a2140467f457f5d5f56ef8c5cd
lyloff <101525885+lyloff@users.noreply.github.com>
2022-04-22 20:55:06 +0200

    Add files via upload

--- commands/Image/challenger.js
+++ commands/Image/challenger.js
@@ -1,23 +1,23 @@
 const Discord = require("discord.js")
 const AmeClient = require("amethyste-api");
 module.exports = {
-  name: "3000yr",
-  aliases: ["3000yrs", "3000"],
+  name: "challenger",
+  aliases: [],
   execute: async(client, message, args, data, db) => {
  
 let AmeAPI = new AmeClient(client.config.imageapi);
 const user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
  
-		const buffer = await AmeAPI.generate("3000years", { url: user.displayAvatarURL({ format: "png", size: 512 }) });
-		const attachment = new Discord.MessageAttachment(buffer, "approved.png");
+		const buffer = await AmeAPI.generate("challenger", { url: user.displayAvatarURL({ format: "png", size: 512 }) });
+		const attachment = new Discord.MessageAttachment(buffer, "challenger.png");
 	
 		message.channel.send(attachment);
 
 	}
 }
   module.exports.help = {
-    name: "3000yrs",
-    description: "Sends a 3000yrs photo of mentioned person ",
-    usage: "3000yrs",
+    name: "challenger",
+    description: "Sends a challenger photo of mentioned person ",
+    usage: "challenger",
     type: "Image" 
 }
--- commands/Image/magik.js
+++ commands/Image/magik.js
@@ -1,14 +1,14 @@
 const Discord = require("discord.js")
 const AmeClient = require("amethyste-api");
 module.exports = {
-  name: "3000yr",
-  aliases: ["3000yrs", "3000"],
+  name: "magik",
+  aliases: [],
   execute: async(client, message, args, data, db) => {
  
 let AmeAPI = new AmeClient(client.config.imageapi);
 const user = message.mentions.users.first() || client.users.cache.get(args[0]) || message.author;
  
-		const buffer = await AmeAPI.generate("3000years", { url: user.displayAvatarURL({ format: "png", size: 512 }) });
+		const buffer = await AmeAPI.generate("magik", { url: user.displayAvatarURL({ format: "png", size: 512 }) });
 		const attachment = new Discord.MessageAttachment(buffer, "approved.png");
 	
 		message.channel.send(attachment);
@@ -16,8 +16,8 @@ const user = message.mentions.users.first() || client.users.cache.get(args[0]) |
 	}
 }
   module.exports.help = {
-    name: "3000yrs",
-    description: "Sends a 3000yrs photo of mentioned person ",
-    usage: "3000yrs",
+    name: "magik",
+    description: "Sends a magik photo of mentioned person ",
+    usage: "magik",
     type: "Image" 
 }
