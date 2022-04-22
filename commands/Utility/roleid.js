1c4c40d642235520be7767749e1ab08420e9077a
lyloff <101525885+lyloff@users.noreply.github.com>
2022-04-22 20:57:18 +0200

    Add files via upload

--- commands/Utility/roleid.js
+++ commands/Utility/roleid.js
@@ -1,24 +1,24 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "id",
-  aliases: ["userid"],
+  name: "roleid",
+  aliases: [],
   execute: async(client, message, args, data, db) => {
-   var mention = message.guild.member(message.mentions.users.first());
+    var mention = message.mentions.roles.first();
     if(!mention) return message.channel.send({embed: {
             color: 16734039,
             description: "Mention a user to get their ID!"
         }})
     const lolid = new Discord.MessageEmbed()
-    .setThumbnail(mention.user.avatarURL)
+   
     .setColor("RANDOM")
-    .addField('Here is ' + `${mention.user.username}\'s ID`, mention.id)
+    .addField('Here is ' + `${mention}`, mention.id)
     message.channel.send(lolid)  
 }
 }
 module.exports.help = {
-    name: "id",
-    description: "Display a user ID",
-    usage: "id",
+    name: "roleid",
+    description: "Display a role ID",
+    usage: "roleid",
     type: "Utility"  
 }
