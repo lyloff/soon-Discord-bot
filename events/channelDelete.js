484a90df21df6a79f61f93acd2e0bdb27f3cf99c
lyloff <101525885+lyloff@users.noreply.github.com>
2022-04-22 21:17:31 +0200

    Add files via upload

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -3,7 +3,6 @@ const config = require("../config");
 
 module.exports = async (client, channel) => {
  try {
-
   if (!channel.guild) return;
   if (!channel.guild.member(client.user).hasPermission("EMBED_LINKS", "VIEW_CHANNEL", "READ_MESSAGE_HISTORY", "VIEW_AUDIT_LOG", "SEND_MESSAGES")) return;
   const log = channel.guild.channels.cache.find(log => log.name === "dumb-logs")
@@ -18,20 +17,21 @@ module.exports = async (client, channel) => {
    } else if (channel.type === "category") {
     var type = "Category";
    } else if (channel.type === "news") {
-     var type = "News Feed";
+    var type = "News Feed";
    } else if (channel.type === "store") {
-     var type = "Store channel";
+    var type = "Store channel";
    } else if (!channel.type) {
-     var type = "?";
+    var type = "?";
    }
    channel.guild.fetchAuditLogs().then(logs => {
-    const userid = logs.entries.first().executor.id;
-    const uavatar = logs.entries.first().executor.avatarURL();
+    var userid = logs.entries.first().executor.id;
+    var uavatar = logs.entries.first().executor.avatarURL();
     const event = new Discord.MessageEmbed()
-     .setTitle("Channel Created")
+     .setTitle("Channel Deleted")
      .setThumbnail(uavatar)
-     .addField("Channel name", `<#${channel.id}> (ID: ${channel.id})`)
+     .addField("Channel name", `${channel.name} (ID: ${channel.id})`)
      .addField("Channel type", `${type}`)
+     .addField("Created at", `${channel.createdAt}`)
      .addField("Created by", `<@${userid}> (ID: ${userid})`)
      .setColor("RANDOM")
      .setTimestamp()
@@ -40,6 +40,6 @@ module.exports = async (client, channel) => {
    });
   }
  } catch (err) {
-  console.log(err);
+  console.log(err)
  }
 }
